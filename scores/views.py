3604195356d22ab44bb6ea96575d65ad6d3eb88a
chris <chris.geelhoed@gmail.com>
2017-03-02 18:53:18 -0800

    configuring postScore

--- scores/views.py
+++ scores/views.py
@@ -7,26 +7,33 @@ from .utils import generateHighscores
 
 
 def getHighscores(request):
-    #scan the database for a key of the subitted value
-    #if such a key exists, return the highscores, otherwise send an error
+    # scan the database for a key of the subitted value
+    # if such a key exists, return the highscores, otherwise send an error
     API_KEY = request.GET.get('key')
     if Key.objects.filter(key=API_KEY):
         return JsonResponse({'highscores': generateHighscores.get()})
 
-    return JsonResponse({'message': 'must provide a valid key' })
+    return JsonResponse({'message': 'must provide a valid key'})
 
 
 def postScore(request):
-    (API_KEY, log) = ( request.GET.get('key'), request.GET.get('log') )
+    data = (API_KEY, name, score, time, logStr) = (
+        request.GET.get('key'),
+        request.GET.get('name'),
+        request.GET.get('score'),
+        request.GET.get('time'),
+        request.GET.get('log'),
+    )
+    if Key.objects.filter(key=API_KEY) and None not in data: #check that API is valid and all data is present
+        # log = json.loads(logStr) #convert from string to dict if needed later
+        #put game log into database
+        Log.objects.create(
+            name = name,
+            score = score,
+            time = time,
+            competing = True,
+            log = logStr,
+        )
+        return JsonResponse({'highscores': generateHighscores.get()}) ##return updated highscore
 
-    log = json.loads(log) #convert from string to dict
-    cats = []
-    for score in log:
-        cats.append(score["category"])
-
-    
-    if Key.objects.filter(key=API_KEY):
-        return JsonResponse({'message': str(cats) })
-
-
-    return JsonResponse({'message': 'must post a valid key' })
+    return JsonResponse({'message': 'must post a valid key'})
