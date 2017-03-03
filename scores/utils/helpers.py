dfc2e29b8a5ef98bbf0dc243d326bc1bedbd9760
chris <chris.geelhoed@gmail.com>
2017-03-02 22:30:14 -0800

    fixed bug, now cleaning up

--- scores/utils/helpers.py
+++ scores/utils/helpers.py
@@ -10,28 +10,6 @@ def sort(scores):  # takes in a list
 
     return scores
 
-'''
-def makeObjectList(model):
-    print("hello from makeObjectList!")
-    print("type", type(model))
-    print("length", len(model))
-    # this converts a model object to a list of objects to be used
-    if type(model) is not list:
-        print("not a list!")
-        model = [ model ]
-    objectList = []
-    for object in model:
-        objectList.append({
-            'name': object.name,
-            'score': object.score,
-            'time': object.time,
-            'id': object.id,  # this id will be used to determine which score matches the user
-        })
-    print("hello, objectList", objectList)
-
-    return objectList
-'''
-
 
 def makeObject(model):
     return {
@@ -40,25 +18,3 @@ def makeObject(model):
         'time': model.time,
         'id': model.id,  # this id will be used to determine which score matches the user
     }
-
-
-'''
-def getScores():
-
-    highscores = []
-    # make an array of score objects
-    for object in models.Log.objects.filter(competing=True):
-        highscores.append({
-            'name': object.name,
-            'score': object.score,
-            'time': object.time,
-            'id': object.id,  # this id will be used to determine which score matches the user
-        })
-
-    # this algorithm sorts with priority on highest score and next on lowest
-    # time
-    highscores = sorted(highscores, key=itemgetter('time'))
-    highscores = sorted(highscores, key=itemgetter('score'), reverse=True)
-
-    return highscores
-'''
--- scores/views.py
+++ scores/views.py
@@ -5,20 +5,7 @@ import json
 
 from .utils import helpers
 
-'''
-def getHighscores(request):
-    # scan the database for a key of the subitted value
-    # if such a key exists, return the highscores, otherwise send an error
-    API_KEY = request.GET.get('key')
-    if Key.objects.filter(key=API_KEY):
-        return JsonResponse({'highscores': generateHighscores.get()})
-
-    return JsonResponse({'message': 'must provide a valid key'})
-'''
-
-
 def postScore(request):
-    print("hello world!")
     data = (API_KEY, name, score, time, logStr) = (
         request.GET.get('key'),
         request.GET.get('name', 'You'),
@@ -26,10 +13,8 @@ def postScore(request):
         request.GET.get('time'),
         request.GET.get('log'),
     )
-    print("hello data!", data)
     # check that API is valid and all data is present
     if Key.objects.filter(key=API_KEY) and None not in data:
-        print("Hello if statement!")
         # log = json.loads(logStr) #convert from string to dict if needed later
         # put game log into database
         Log.objects.create(
@@ -40,30 +25,17 @@ def postScore(request):
             log=logStr,
         )
 
-        print("hello post database push")
-
         # next get players model
         playerModel = Log.objects.filter(competing=True).order_by('-id')[0]
-        print("hello playerModel", playerModel)
         # then conver to an object
         playerObject = helpers.makeObject(playerModel)
-        print("hello playerObject", playerObject)
         # then grab playerId
         playerId = playerObject['id']
-        print("hello id!", playerId)
 
         scoreObjects = []
         for model in Log.objects.filter(competing=True).exclude(name='You'):
             scoreObjects.append(helpers.makeObject(model))
 
-        print("hello scoreObjects", scoreObjects)
-
-        '''
-        # now get all scores where players are competing and name is not 'You'
-        scoreModels = Log.objects.filter(competing=True).exclude(name='You')
-        # convert to a list of objects
-        scoreObjects = helpers.makeObjectList(scoreModels)
-        '''
 
         # now combine add the player object to the score objects and sort
         scoreObjects.append(playerObject)
